d84bf083e77436015d0f63ee05e20c8dd081776e
Tanner <troxs690@gmail.com>
2023-02-06 15:53:13 -0600

    d

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,7 +8,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(permitted_params)
     render json: bird, status: :created
   end
 
@@ -22,4 +22,9 @@ class BirdsController < ApplicationController
     end
   end
 
+  private
+
+  def permitted_params
+    params.permit(:name, :species)
+  end
 end
